3e332b13804925cb6438d8cd181f16fdcaa3e5dd
PavelShep <71721882+PavelShep@users.noreply.github.com>
2020-09-25 21:01:26 +0300

    Add files via upload

---
+++ Task2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            for (int i=1; i<=N; i++){
+                sum = sum + i;
+            }
+
+            Console.WriteLine("Cумма всех натуральных чисел от 1 до " + N + ": " + sum);
+        }
+    }
+}
