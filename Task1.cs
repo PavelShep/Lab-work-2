03d0798674d8334de4610877d9252ddc227a7684
PavelShep <71721882+PavelShep@users.noreply.github.com>
2020-09-25 20:39:31 +0300

    Add files via upload

---
+++ Task1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            if (b < 0){
+                b = -b;
+            }
+
+            for (int i=1; i<=b; i++){
+                sum = sum + a;
+            }
+
+            Console.WriteLine("Произведение: " + sum);
+        }
+    }
+}
